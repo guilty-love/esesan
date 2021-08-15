3bcf7d9fe2a4b47321ff146440c1d56ce768c88a
魂牵梦萦 <54766361+guilty-love@users.noreply.github.com>
2021-08-15 21:16:08 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -36,11 +36,9 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
 
  //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-
-  'rwgatbt5ix7svzkz4ucmdwq4m4@4vvbjlml6tdfcedqjsubcpsmwadduaqgezjh62y@mlrdw3aw26j3xrtokbx4gpbf3x3xu6eylscaaty@gcdr655xfdjq7rwleo6evhzuqcupgflqru36bua@suqg5cye47cqmyg3dl4mbkcjs4d5nu6pod62iha',
-
-  //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'rwgatbt5ix7svzkz4ucmdwq4m4@4vvbjlml6tdfcedqjsubcpsmwadduaqgezjh62y@mlrdw3aw26j3xrtokbx4gpbf3x3xu6eylscaaty@gcdr655xfdjq7rwleo6evhzuqcupgflqru36bua@suqg5cye47cqmyg3dl4mbkcjs4d5nu6pod62iha',
+'rwgatbt5ix7svzkz4ucmdwq4m4@4vvbjlml6tdfcedqjsubcpsmwadduaqgezjh62y@mlrdw3aw26j3xrtokbx4gpbf3x3xu6eylscaaty@gcdr655xfdjq7rwleo6evhzuqcupgflqru36bua@suqg5cye47cqmyg3dl4mbkcjs4d5nu6pod62iha',
+ //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
+ 'rwgatbt5ix7svzkz4ucmdwq4m4@4vvbjlml6tdfcedqjsubcpsmwadduaqgezjh62y@mlrdw3aw26j3xrtokbx4gpbf3x3xu6eylscaaty@gcdr655xfdjq7rwleo6evhzuqcupgflqru36bua@suqg5cye47cqmyg3dl4mbkcjs4d5nu6pod62iha',
 ]
 let allMessage = ``;
 let currentRoundId = null;//本期活动id
